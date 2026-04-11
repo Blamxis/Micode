1291fcaeccc6f0124eb94770779985ef27721bc0
Maxime Gavinet <maxgvt26@icloud.com>
2026-04-11 16:24:26 +0200

    Initial project setup

---
+++ src/main/java/com/micode/micode/MicodeApplication.java
@@ -0,0 +1,13 @@
+package com.micode.micode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/micode/micode/MicodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micode.micode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
