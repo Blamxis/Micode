72dbec68140de90bacbccd4176fdb22a778f07fc
Maxime Gavinet <maxgvt26@icloud.com>
2026-04-14 17:59:29 +0200

    Add rate limiting per IP and integrate client IP handling

--- src/main/java/com/micode/micode/controller/AuthController.java
+++ src/main/java/com/micode/micode/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.micode.micode.controller;
 
 import com.micode.micode.dto.RegisterRequest;
+import com.micode.micode.dto.RegisterResponse;
 import com.micode.micode.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
-
 public class AuthController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public void register(@RequestBody RegisterRequest registerRequest) {}
+    public RegisterResponse register(@Valid @RequestBody RegisterRequest registerRequest, HttpServletRequest httpRequest) {
+        // --- Récupération de l'IP réelle ( compatible proxy / nginx )
+        String clientIp = httpRequest.getHeader("X-Forwarded-For");
+        if (clientIp == null) {
+            clientIp = httpRequest.getRemoteAddr();
+        }
+        return userService.register(registerRequest, clientIp);
+    }
 }
