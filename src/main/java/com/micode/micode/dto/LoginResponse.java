d89f90af38da4ffe68644be372016329593f6ed4
Maxime Gavinet <maxgvt26@icloud.com>
2026-04-16 18:02:10 +0200

    Add login DTOs (LoginRequest & LoginResponse)

---
+++ src/main/java/com/micode/micode/dto/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.micode.micode.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+    private String message;
+    private String token;
+    private String email;
+    private String username;
+}
