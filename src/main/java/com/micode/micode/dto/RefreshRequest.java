97d6eb1980e33e32e8bc0784689301d4495a8ed0
Maxime Gavinet <maxgvt26@icloud.com>
2026-04-18 15:01:53 +0200

    Add DTOs for refresh token endpoint

---
+++ src/main/java/com/micode/micode/dto/RefreshRequest.java
@@ -0,0 +1,8 @@
+package com.micode.micode.dto;
+
+import lombok.Data;
+
+@Data
+public class RefreshRequest {
+    private String refreshToken;
+}
---
+++ src/main/java/com/micode/micode/dto/RefreshResponse.java
@@ -0,0 +1,11 @@
+package com.micode.micode.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RefreshResponse {
+    private String accessToken;
+    private String refreshToken;
+}
