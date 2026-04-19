c2f158e986b06ff982e49271baee401ce456efa7
Maxime Gavinet <maxgvt26@icloud.com>
2026-04-19 15:47:26 +0200

    refactor(controller): update AuthController to use new auth/register services

--- src/main/java/com/micode/micode/controller/AuthController.java
+++ src/main/java/com/micode/micode/controller/AuthController.java
@@ -4,8 +4,9 @@ import com.micode.micode.dto.*;
 import com.micode.micode.model.RefreshToken;
 import com.micode.micode.model.User;
 import com.micode.micode.security.JwtService;
+import com.micode.micode.service.AuthService;
 import com.micode.micode.service.RefreshTokenService;
-import com.micode.micode.service.UserService;
+import com.micode.micode.service.RegisterService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,18 +20,21 @@ import org.springframework.web.server.ResponseStatusException;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
-    private final UserService userService;
+    private final RegisterService registerService;
     private final RefreshTokenService refreshTokenService;
     private final JwtService jwtService;
+    private final AuthService authService;
 
     @PostMapping("/register")
     public RegisterResponse register(@Valid @RequestBody RegisterRequest registerRequest, HttpServletRequest httpRequest) {
-        // --- Récupération de l'IP réelle ( compatible proxy / nginx )
+
         String clientIp = httpRequest.getHeader("X-Forwarded-For");
+
         if (clientIp == null) {
             clientIp = httpRequest.getRemoteAddr();
         }
-        return userService.register(registerRequest, clientIp);
+
+        return registerService.register(registerRequest, clientIp);
     }
 
     @PostMapping("/login")
@@ -39,13 +43,13 @@ public class AuthController {
         if (clientIp == null) {
             clientIp = httpRequest.getRemoteAddr();
         }
-        return userService.login(loginRequest, clientIp);
+        return authService.login(loginRequest, clientIp);
     }
 
     @GetMapping("/me")
     public RegisterResponse me(Authentication authentication) {
         String email = authentication.getName();
-        var user = userService.findByEmail(email);
+        var user = authService.findByEmail(email);
 
         return new RegisterResponse(
                 "User authenticated",
@@ -95,9 +99,9 @@ public class AuthController {
         }
 
         String email = authentication.getName();
-        User user = userService.findByEmail(email);
+        User user = authService.findByEmail(email);
 
-        userService.logout(user);
+        authService.logout(user);
 
         return ResponseEntity.ok("Logged out successfully");
     }
@@ -112,7 +116,7 @@ public class AuthController {
         }
 
         String email = authentication.getName();
-        userService.changePassword(email, request);
+        authService.changePassword(email, request);
 
         return new ChangePasswordResponse("Password updated successfully");
     }
